f71a593de109178deee9a9838d596b0f935e85a5
Nickolashaa <gracevnikolaj220@gmail.com>
2025-03-31 02:32:27 +0300

    Работа с json

---
+++ jsonManager/manager.go
@@ -0,0 +1,69 @@
+package jsonManager
+
+import (
+	"fmt"
+	"encoding/json"
+	"Sinekod/models"
+	"Sinekod/storage"
+)
+
+
+func Get_json_id(id string) []byte {		 //любой вывод json id
+	var choto = map[string]string{"id": id}
+	data, err := json.Marshal(choto)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return data
+}
+
+func Get_json_books() []byte { 		//GET /books
+	data, err := json.Marshal(storage.Books)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return data
+}
+
+func Get_json_books_id(id string) ([]byte, string) {		 //GET /books/{id}
+	value, ok := storage.Books[id]
+	if !ok {
+		return nil, "404 not found"
+	}
+	data, err := json.Marshal(value)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return data, "200"
+}
+
+func Post_json_users(id string, data []byte) ([]byte, string) {
+	new_user := models.User{}
+	err := json.Unmarshal(data, &new_user)
+	if err != nil{
+		fmt.Println(err)
+	}
+	s := new_user.Email
+	flag := false
+	for i := 0;i < len(s); i++{
+		if string(s[i]) == "@"{
+			flag = true
+			break
+		}
+	}
+	if !flag{
+		fmt.Println("Некорректный email")
+	}
+	storage.Users[id] = new_user
+	return Get_json_id(id), "201 Created"
+}
+
+func Post_json_books(id string, data []byte) ([]byte, string) {
+	new_book := models.Book{}
+	err := json.Unmarshal(data, &new_book)
+	if err != nil{
+		fmt.Println(err)
+	}
+	storage.Books[id] = new_book
+	return Get_json_id(id), "201 Created"
+}
